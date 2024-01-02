2270b839ebbbeaebfbda4ba04895aedecb53ca80
Kimjinwoo1 <104746237+Kimjinwoo1@users.noreply.github.com>
2024-01-02 13:01:27 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,19 @@
+function solution(num_list) {
+    let result;
+
+    if (num_list.length >= 11) {
+
+        result = 0;
+        for (let i = 0; i < num_list.length; i++) {
+            result += num_list[i];
+        }
+    } else {
+
+        result = 1;
+        for (let i = 0; i < num_list.length; i++) {
+            result *= num_list[i];
+        }
+    }
+
+    return result;
+}

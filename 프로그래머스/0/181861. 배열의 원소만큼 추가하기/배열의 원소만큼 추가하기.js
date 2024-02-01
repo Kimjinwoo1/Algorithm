8c313ef3aee47635cd9c5b4924fbea584c1dc8d4
Kimjinwoo1 <104746237+Kimjinwoo1@users.noreply.github.com>
2024-02-01 17:43:39 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    let answer = [];
+    
+    for(let i=0; i < arr.length; i++){
+        let natNum = arr[i]
+    
+    for(let j=0; j< natNum; j++){
+        answer.push(natNum)
+    }
+    }
+    return answer;
+}
